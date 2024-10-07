0c9c750b5d869571f57c27503a5777d89210aa51
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-10-07 12:36:11 -0300

    commented Linhas hook

--- src/hooks/Linhas/index.tsx
+++ src/hooks/Linhas/index.tsx
@@ -1,43 +1,43 @@
-import { api } from '@/libs/api';
-import { AxiosError } from 'axios';
-import React from 'react';
+// import { api } from '@/libs/api';
+// import { AxiosError } from 'axios';
+// import React from 'react';
 
-interface LinhasContextProps {
-	linhas: Linha[];
-	categorias: Categoria[];
-}
+// interface LinhasContextProps {
+// 	linhas: Linha[];
+// 	categorias: Categoria[];
+// }
 
-const LinhasContext = React.createContext({} as LinhasContextProps);
+// const LinhasContext = React.createContext({} as LinhasContextProps);
 
-export const LinhasHook = ({ children }: React.PropsWithChildren) => {
-	const [linhas, setLinhas] = React.useState<Linha[]>([]);
-	const [categorias, setCategorias] = React.useState<Categoria[]>([]);
+// export const LinhasHook = ({ children }: React.PropsWithChildren) => {
+// 	const [linhas, setLinhas] = React.useState<Linha[]>([]);
+// 	const [categorias, setCategorias] = React.useState<Categoria[]>([]);
 
-	React.useEffect(() => {
-		api
-			.get<Linha[]>('/api/v1/lines')
-			.then((res) =>
-				setLinhas(res.data.sort((a, b) => (a.nome < b.nome ? -1 : 1))),
-			)
-			.catch((err) => alert((err as AxiosError).response?.status));
-		api
-			.get<Categoria[]>('/api/v1/categories')
-			.then((res) => setCategorias(res.data))
-			.catch((err) => alert((err as AxiosError).message));
-	}, []);
+// 	React.useEffect(() => {
+// 		api
+// 			.get<Linha[]>('/api/v1/lines')
+// 			.then((res) =>
+// 				setLinhas(res.data.sort((a, b) => (a.nome < b.nome ? -1 : 1))),
+// 			)
+// 			.catch((err) => alert((err as AxiosError).response?.status));
+// 		api
+// 			.get<Categoria[]>('/api/v1/categories')
+// 			.then((res) => setCategorias(res.data))
+// 			.catch((err) => alert((err as AxiosError).message));
+// 	}, []);
 
-	return (
-		<LinhasContext.Provider
-			value={{
-				linhas,
-				categorias,
-			}}
-		>
-			{children}
-		</LinhasContext.Provider>
-	);
-};
+// 	return (
+// 		<LinhasContext.Provider
+// 			value={{
+// 				linhas,
+// 				categorias,
+// 			}}
+// 		>
+// 			{children}
+// 		</LinhasContext.Provider>
+// 	);
+// };
 
-export function useLinhas() {
-	return React.useContext(LinhasContext);
-}
+// export function useLinhas() {
+// 	return React.useContext(LinhasContext);
+// }
